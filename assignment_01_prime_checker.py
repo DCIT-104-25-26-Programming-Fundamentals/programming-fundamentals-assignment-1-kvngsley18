49399c82580f8663d9fcb0960e2787fe26ba5b9b
kvngsley18 <kingsleydebrah025@gmail.com>
2026-07-26 11:56:58 +0000

    Completed Prime Number Checker task

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -43,15 +43,13 @@ def is_prime(n):
     if n < 2:
         return False
     
-    # Check for factors from 2 up to the square root of n
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            return False  # Found a divisor, so it's not prime
+            return False 
             
     return True
 
 
-# Main program
 if __name__ == "__main__":
     num = int(input("Enter a number: "))
     
